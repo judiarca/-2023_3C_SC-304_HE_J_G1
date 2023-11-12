6f309084ec23ba44aec43fe63d8d0fc261954b30
moisfonb <147448114+moisfonb@users.noreply.github.com>
2023-11-12 16:33:25 -0600

    Creación de métodos en la clase Vagón

--- ProyectoGrupo1/src/proyectogrupo1/Vagon.java
+++ ProyectoGrupo1/src/proyectogrupo1/Vagon.java
@@ -9,5 +9,51 @@ package proyectogrupo1;
  * @author moise
  */
 public class Vagon {
-    
-}
+
+    private Pila[] asientos;
+    private int capacidadMaxima;
+
+    public Vagon(int numFilas, int asientosPorFila) {
+        this.asientos = new Pila[numFilas];
+        this.capacidadMaxima = numFilas * asientosPorFila;
+
+        for (int i = 0; i < numFilas; i++) {
+            asientos[i] = new Pila(asientosPorFila);
+        }
+    }
+
+    public boolean subirPasajero(Pasajero pasajero) {
+        for (Pila fila : asientos) {
+            if (!fila.estaLlena()) {
+                fila.apilar(pasajero);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String imprimirEstadoVagon() {
+        StringBuilder resultado = new StringBuilder();
+
+        resultado.append("Estado Actual del Vagon\n");
+
+        for (int i = 0; i < asientos.length; i++) {
+            resultado.append("Fila ").append(i + 1).append(": ");
+            resultado.append(asientos[i].imprimirPila()).append("\n");
+        }
+
+        resultado.append("Capacidad Máxima: ").append(capacidadMaxima).append("\n");
+        resultado.append("Espacios Disponibles: ").append(obtenerEspaciosDisponibles()).append("\n");
+
+        return resultado.toString();
+    }
+
+    public int obtenerEspaciosDisponibles() {
+        int espaciosDisponibles = 0;
+        for (Pila fila : asientos) {
+            espaciosDisponibles += fila.contarCapacidad();
+        }
+        return espaciosDisponibles;
+    }
+
+}//Final de la clase 
